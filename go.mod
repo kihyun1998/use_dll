c9b3a69d3d39580e711496a033ea4c74bca1d0e2
박기현 <kihyun1998@naver.com>
2025-04-17 13:47:48 +0900

    FEAT: 간단한 go dll 구현

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kihyun1998/use_dll
+
+go 1.22.2
---
+++ mylib.go
@@ -0,0 +1,23 @@
+package main
+
+import "C"
+
+//export CallBasic2
+func CallBasic2(key, msg *C.char) (resultMsg *C.char, isOk bool) {
+	// key와 msg를 Go 문자열로 변환
+	goKey := C.GoString(key)
+	goMsg := C.GoString(msg)
+
+	// 실제 비즈니스 로직 구현
+	if goKey == "valid_key" {
+		isOk = true
+		resultMsg = C.CString("Success: " + goMsg)
+	} else {
+		isOk = false
+		resultMsg = C.CString("Failed: unvalid key.")
+	}
+
+	return
+}
+
+func main() {}
